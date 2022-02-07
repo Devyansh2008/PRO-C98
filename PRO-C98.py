b4504ab8bcf05c140ebf3c274448d703729d9e0e
Devyansh2008 <73015511+Devyansh2008@users.noreply.github.com>
2022-02-07 14:08:51 +0530

    Update PRO-C98.py

--- PRO-C98.py
+++ PRO-C98.py
@@ -1,12 +1,15 @@
-a=input("What is the path of the first file?,\n")
-b=input("What is the path of the second file?,\n")
+def ProC98() :
+    a=input("What is the path of the first file?,\n")
+    b=input("What is the path of the second file?,\n")
 
-c1=open(a)
-readc1=c1.read()
-c2=open(b)
-readc2=c2.read()
+    c1=open(a)
+    readc1=c1.read()
+    c2=open(b)
+    readc2=c2.read()
 
-c1=open(a,"w")
-c1.write(readc2)
-c2=open(b,"w")
-c2.write(readc1)
+    c1=open(a,"w")
+    c1.write(readc2)
+    c2=open(b,"w")
+    c2.write(readc1)
+
+ProC98()
